ac2512fd890802745bda425e53b64a20473821df
snailspot <kousukekuno@hotmail.com>
2025-07-09 23:38:10 +0930

    Created JS scripts to display current week in split view as well as date next to each day of the week

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,19 +8,23 @@ function getDate(){
         // Declaring variable for date display, getting date from system
         const fullDate = new Date();
         // Formats date string into separate date and time strings
-        const date = formatDate(fullDate);
+        const date = formatHeaderDate(fullDate);
         // Updates date/time <p> elements in navigation bar by ID
         document.getElementById("date").innerHTML = date;
 }
 
-function formatDate(fullDate){
+function formatHeaderDate(fullDate){
     const year = fullDate.getFullYear();
     const month = fullDate.getMonth()+1;
     const day = fullDate.getDate();
 
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-    const date = weekday[fullDate.getDay()] + " " + day.toString().concat("/",month.toString(),"/", year.toString());
+    const date = weekday[fullDate.getDay()] + " " + formatDateStr(day).concat("/",formatDateStr(month),"/", year.toString());
 
     return date;
+}
+
+function formatDateStr(date){
+    return date < 10 ? "0" + date.toString() : date.toString();
 }
